52d858351981b50c651e54f92bd70617705dbbd8
yudhistiroagung <yudhistiro.agung@gmail.com>
2026-05-12 09:05:09 +0700

    how to domain

--- src/data/di/index.ts
+++ src/data/di/index.ts
@@ -1,6 +1,7 @@
 import { container } from 'tsyringe';
 
 import { AppDatabase } from '@/cores/dexie/db-dexie';
+
 import TodoLocalDb from '../todos/datasources/local/db';
 import { TodoLocalDatasource } from '../todos/datasources/local/todo-local-datasource';
 import { TodoRemoteDatasource } from '../todos/datasources/remote/todo-remote-datasource';
